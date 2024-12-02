c65789e5be28d54f18711c92d75de9d50718855d
kk0425 <kk0425@gmail.com>
2024-12-01 23:16:18 -0500

    aoc day 1 part 1 solved

---
+++ day_1.ts
@@ -0,0 +1,51 @@
+import { assertEquals } from "@std/assert";
+import { runPart } from "@macil/aocd";
+
+function parse(input: string): Array<[number, number]> {
+  return input.trimEnd().split("\n").map((line) => {
+    return line.split(/\s+/).map(Number) as [number, number];
+  });
+}
+
+function part1(input: string): number {
+  const items = parse(input);
+  const leftList = items
+    .map((item) => item[0])
+    .sort((a, b) => a - b);
+  const rightList = items
+    .map((item) => item[1])
+    .sort((a, b) => a - b);
+
+  let totalDistance = 0;
+  for (let i = 0; i < items.length; i++) {
+    totalDistance += Math.abs(leftList[i] - rightList[i]);
+  }
+  return totalDistance;
+}
+
+// function part2(input: string): number {
+//   const items = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2024, 1, 1, part1);
+  // runPart(2024, 1, 2, part2);
+}
+
+const TEST_INPUT = `\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 11);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
