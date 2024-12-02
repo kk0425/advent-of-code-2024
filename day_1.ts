dc772585f102ffda4486bd9723606168bf7434c7
kk0425 <kk0425@gmail.com>
2024-12-01 23:33:15 -0500

    day 1 part 2 solution

--- day_1.ts
+++ day_1.ts
@@ -23,14 +23,29 @@ function part1(input: string): number {
   return totalDistance;
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function part2(input: string): number {
+  const items = parse(input);
+  const leftList = items
+    .map((item) => item[0]);
+  const rightList = items
+    .map((item) => item[1]);
+
+  let totalSimilarityScore = 0;
+  for (const leftItem of leftList) {
+    let count = 0;
+    for (const rightItem of rightList) {
+      if (rightItem === leftItem) {
+        count++;
+      }
+    }
+    totalSimilarityScore += leftItem * count;
+  }
+  return totalSimilarityScore;
+}
 
 if (import.meta.main) {
   runPart(2024, 1, 1, part1);
-  // runPart(2024, 1, 2, part2);
+  runPart(2024, 1, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -46,6 +61,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 11);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 31);
+});
