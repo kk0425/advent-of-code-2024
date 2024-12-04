1637c9806fab2ec22b0c332c8a90a61979df4b30
kk0425 <kk0425@gmail.com>
2024-12-03 23:35:13 -0500

    day 2 part 2

--- day_2.ts
+++ day_2.ts
@@ -35,14 +35,28 @@ function part1(input: string): number {
   return safeReports.length;
 }
 
-// function part2(input: string): number {
-//   const reports = parse(input);
-//   throw new Error("TODO");
-// }
+function checkReportDampener(report: Array<number>): boolean {
+  if (checkReport(report)) {
+    return true;
+  }
+  for (let i = 0; i < report.length; i++) {
+    const modifiedReport = report.filter((_value, index) => index !== i);
+    if (checkReport(modifiedReport)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function part2(input: string): number {
+  const reports = parse(input);
+  const safeReports = reports.filter(checkReportDampener);
+  return safeReports.length;
+}
 
 if (import.meta.main) {
   runPart(2024, 2, 1, part1);
-  // runPart(2024, 2, 2, part2);
+  runPart(2024, 2, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -58,6 +72,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 2);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 4);
+});
