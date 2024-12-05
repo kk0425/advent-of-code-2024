429c0ef2ebd762665bf76bf4e1e1e7dffe4f134d
kk0425 <kk0425@gmail.com>
2024-12-04 23:06:40 -0500

    day 3 part 2

--- day_3.ts
+++ day_3.ts
@@ -11,14 +11,26 @@ function part1(input: string): number {
   return totalSum;
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function part2(input: string): number {
+  const regex = /mul\((\d+),(\d+)\)|don't\(\)|do\(\)/g;
+  const matches = input.matchAll(regex);
+  let totalSum = 0;
+  let multEnabled = true;
+  for (const match of matches) {
+    if (match[0] === "don't()") {
+      multEnabled = false;
+    } else if (match[0] === "do()") {
+      multEnabled = true;
+    } else if (multEnabled) {
+      totalSum += Number(match[1]) * Number(match[2]);
+    }
+  }
+  return totalSum;
+}
 
 if (import.meta.main) {
   runPart(2024, 3, 1, part1);
-  // runPart(2024, 3, 2, part2);
+  runPart(2024, 3, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -29,6 +41,11 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 161);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(
+    part2(
+      "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    ),
+    48,
+  );
+});
