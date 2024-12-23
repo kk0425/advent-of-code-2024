6fe37614754e4aef96021dbb8f616c9064cf5733
kk0425 <kk0425@gmail.com>
2024-12-23 01:34:11 -0500

    day 4 part 2

--- day_4.ts
+++ day_4.ts
@@ -12,6 +12,7 @@ function part1(input: string): number {
   const searchStr = "XMAS";
   for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
     for (let columnIndex = 0; columnIndex < rows[rowIndex].length; columnIndex++) {
+      const location: Vector2 = [rowIndex, columnIndex];
       const stepCombos: Vector2[] = [
         [0, 1],
         [0, -1],
@@ -25,7 +26,6 @@ function part1(input: string): number {
       for (const step of stepCombos) {
         let isMatch = true;
         for (let searchIndex = 0; searchIndex < searchStr.length; searchIndex++) {
-          const location: Vector2 = [rowIndex, columnIndex];
           if (
             searchStr[searchIndex] !==
               getLocationFromStringGrid(
@@ -46,17 +46,73 @@ function part1(input: string): number {
   return xmasCount;
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function part2(input: string): number {
+  const rows = parse(input);
+  let xmasCount = 0;
+  for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < rows[rowIndex].length; columnIndex++) {
+      const location: Vector2 = [rowIndex, columnIndex];
+      if (getLocationFromStringGrid(rows, location) !== "A") {
+        continue;
+      }
+
+      const upLeftNeighbor: Vector2 = [rowIndex - 1, columnIndex - 1];
+      const upLeftNeighborValue = getLocationFromStringGrid(rows, upLeftNeighbor);
+      if (upLeftNeighborValue !== "M" && upLeftNeighborValue !== "S") {
+        continue;
+      }
+
+      const downRightNeighbor: Vector2 = [rowIndex + 1, columnIndex + 1];
+      const downRightNeighborValue = getLocationFromStringGrid(rows, downRightNeighbor);
+      const expectedDownRightNeighborValue = upLeftNeighborValue === "M" ? "S" : "M";
+      if (downRightNeighborValue !== expectedDownRightNeighborValue) {
+        continue;
+      }
+
+      const upRightNeighbor: Vector2 = [rowIndex - 1, columnIndex + 1];
+      const upRightNeighborValue = getLocationFromStringGrid(rows, upRightNeighbor);
+      if (upRightNeighborValue !== "M" && upRightNeighborValue !== "S") {
+        continue;
+      }
+
+      const downLeftNeighbor: Vector2 = [rowIndex + 1, columnIndex - 1];
+      const downLeftNeighborValue = getLocationFromStringGrid(rows, downLeftNeighbor);
+      const expectedDownLeftNeighborValue = upRightNeighborValue === "M" ? "S" : "M";
+      if (downLeftNeighborValue !== expectedDownLeftNeighborValue) {
+        continue;
+      }
+
+      xmasCount++;
+    }
+  }
+  return xmasCount;
+}
 
 if (import.meta.main) {
   runPart(2024, 4, 1, part1);
-  // runPart(2024, 4, 2, part2);
+  runPart(2024, 4, 2, part2);
 }
 
 const TEST_INPUT = `\
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 18);
+});
+
+Deno.test("part2", () => {
+  assertEquals(
+    part2(`\
 MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -67,12 +123,7 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX
-`;
-
-Deno.test("part1", () => {
-  assertEquals(part1(TEST_INPUT), 18);
+`),
+    9,
+  );
 });
-
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
