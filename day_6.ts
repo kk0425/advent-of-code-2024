3ac12647424f1907e21e34348180075c444cb62c
kk0425 <kk0425@gmail.com>
2025-06-16 03:40:45 -0400

    almost finished day 6

--- day_6.ts
+++ day_6.ts
@@ -7,32 +7,59 @@ function parse(input: string): string[] {
 
 function part1(input: string): number {
   const labMap = parse(input);
-  let guardRow: number | undefined;
-  let guardColumn: number | undefined;
+  let guardPosition: [number | undefined, number | undefined] = [0, 0];
 
   //find guard's starting position
   outer: for (let row = 0; row < labMap.length; row++) {
     for (let column = 0; column < labMap[row].length; column++) {
       if (labMap[row][column] === "^") {
-        guardRow = row;
-        guardColumn = column;
+        guardPosition = [row, column];
         break outer;
       }
     }
   }
-  if (guardRow === undefined || guardColumn === undefined) {
+  if (guardPosition[0] === undefined || guardPosition[1] === undefined) {
     throw new Error("Guard not found.");
   }
 
   //move guard and step count logic
   const guardMovement = [[-1, 0], [0, 1], [1, 0], [0, -1]]; //up, right, down, left
-  const guardUniqueSteps = new Set<string>();
+  let guardDirection = 0;
+  const guardUniqueLocations = new Set<string>();
 
-  console.log(labMap[guardRow + guardMovement[3][0]][guardColumn + guardMovement[3][1]]);
+  function addPair(guardPos: [number | undefined, number | undefined], direction: number): [number, number] {
+    const guard = [0, 0];
+    guard[0] = Number(guardPos[0]);
+    guard[1] = Number(guardPos[1]);
 
-  guardUniqueSteps.add(JSON.stringify([guardRow, guardColumn]));
+    guard[0] += guardMovement[direction][0];
+    guard[1] += guardMovement[direction][1];
 
-  return guardUniqueSteps.size;
+    return guard as [number, number];
+  }
+
+  let guardCheck: [number, number] = addPair(guardPosition, guardDirection);
+  let valueAtPosition: string = labMap[guardCheck[0]][guardCheck[1]];
+
+  while (valueAtPosition !== undefined) {
+    guardCheck = addPair(guardPosition, guardDirection);
+    valueAtPosition = labMap[guardCheck[0]][guardCheck[1]]; //crashing here when getting type after out of bounds
+
+    if (valueAtPosition === "#") {
+      if (guardDirection < 3) {
+        guardDirection++;
+      } else {
+        guardDirection = 0;
+      }
+    } else if (valueAtPosition === "." || valueAtPosition === "^") {
+      guardUniqueLocations.add(JSON.stringify([guardPosition]));
+      guardPosition = guardCheck;
+      console.log("guard step: ", guardUniqueLocations.size); //test
+    }
+  }
+
+  console.log("Guard final step count: ", guardUniqueLocations.size + 1);
+  return guardUniqueLocations.size + 1;
 }
 
 // function part2(input: string): number {
